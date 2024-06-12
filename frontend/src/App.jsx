68c1e5de99ced06682373a35db58eb8d8d25c5a7
Merlin Schuckert <merlin.schuckert@stud.htwk-leipzig.de>
2024-06-12 21:45:04 +0200

    feat: Sidebar implemenatation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,34 +9,21 @@ import Dashboard from './components/Dashboard.jsx';
 import NewAudit from './components/NewAudit.jsx';
 import PerformAudit from './components/PerformAudit.jsx';
 import Evaluation from './components/Evaluation.jsx';
+import Sidebar from './components/Sidebar.jsx';
 
 function App() {
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Dashboard</Link>
-            </li>
-            <li>
-              <Link to="/newAudit">NewAudit</Link>
-            </li>
-            <li>
-              <Link to="/performAudit">PerformAudit</Link>
-            </li>
-            <li>
-              <Link to="/evaluation">Evaluation</Link>
-            </li>
-          </ul>
-        </nav>
-
+      <div className="flex">
+      <Sidebar />
+      <div className="ml-64 p-4 w-full">
         <Routes>
           <Route path="/" element={<Dashboard />} />
           <Route path="/newAudit" element={<NewAudit />} />
           <Route path="/performAudit" element={<PerformAudit />} />
           <Route path="/evaluation" element={<Evaluation />} />
         </Routes>
+      </div>
       </div>
     </Router>
   );
