76f1bc35071362a702637f4d051b2402c9381da6
Abolfazl <abolfazl.shahbazfar@stud.htwk-leipzig.de>
2024-06-14 17:06:14 +0200

    package and cypress config edited

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,17 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
+    baseUrl: "http://localhost:3000",
+    //setupNodeEvents(on, config) {
       // implement node event listeners here
-    },
+      // z.B., Screenshot bei Fehlern oder zusätzliche Plugins laden
+    //},
+    supportFile: "cypress/support/index.js",
+    specPattern: "cypress/integration/**/*.spec.js",
+    video: true,
+    screenshotsFolder: "cypress/screenshots",
+    videosFolder: "cypress/videos",
+    viewportWidth: 1280,
+    viewportHeight: 720,
   },
 });
