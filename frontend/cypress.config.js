f5a2570eefff9f17639d28300150c741053fef5b
Abolfazl <abolfazl.shahbazfar@stud.htwk-leipzig.de>
2024-06-15 15:32:34 +0200

    cypress.config fixed verify as commonjs

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -1,9 +1,20 @@
-import { defineConfig } from "cypress";
+// @ts-ignore
+// @cypress/verify/module-type commonjs
+const { defineConfig } = require("cypress");
 
-export default defineConfig({
+module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
+    baseUrl: "http://localhost:5173",
+    //setupNodeEvents(on, config) {
       // implement node event listeners here
-    },
+      // z.B., Screenshot bei Fehlern oder zusätzliche Plugins laden
+    //},
+    supportFile: "cypress/support/index.js",
+    specPattern: "cypress/integration/**/*.spec.js",
+    video: true,
+    screenshotsFolder: "cypress/screenshots",
+    videosFolder: "cypress/videos",
+    viewportWidth: 1280,
+    viewportHeight: 720,
   },
 });
