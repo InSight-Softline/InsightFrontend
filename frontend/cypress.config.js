de4f841acd37eb049d5383cda00a1eb701149a85
Abolfazl <abolfazl.shahbazfar@stud.htwk-leipzig.de>
2024-06-15 15:45:15 +0200

    delete unnecessary configurations

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -5,16 +5,5 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "http://localhost:5173",
-    //setupNodeEvents(on, config) {
-      // implement node event listeners here
-      // z.B., Screenshot bei Fehlern oder zusätzliche Plugins laden
-    //},
-    supportFile: "cypress/support/index.js",
-    specPattern: "cypress/integration/**/*.spec.js",
-    video: true,
-    screenshotsFolder: "cypress/screenshots",
-    videosFolder: "cypress/videos",
-    viewportWidth: 1280,
-    viewportHeight: 720,
   },
 });
