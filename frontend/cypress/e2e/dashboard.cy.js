a7c3e437eefdb9eb2c1250ead313f5a78e012918
Abolfazl <abolfazl.shahbazfar@stud.htwk-leipzig.de>
2024-06-17 13:35:58 +0200

    dashboard test adding Timeout

--- frontend/cypress/e2e/dashboard.cy.js
+++ frontend/cypress/e2e/dashboard.cy.js
@@ -1,60 +1,52 @@
 Cypress.on('uncaught:exception', (err, runnable) => {
-    return false
-  })
-
-
+    return false;
+});
 
 describe('Dashboard Navigation Tests', () => {
     beforeEach(() => {
         cy.visit('http://localhost:5173');
-        cy.wait(5000);  // Wartezeit von 5 Sekunden nach dem Besuch der Seite
+        cy.wait(5000); // Wartezeit von 5 Sekunden nach dem Besuch der Seite
+    });
+
+    afterEach(() => {
+        cy.screenshot(); // Screenshot nach jedem Test
     });
 
     it('should find and click the main links to the subpages', () => {
-        cy.contains('Dashboard').click();
-        cy.url().should('include', 'http://localhost:5173');
+        cy.contains('Dashboard', { timeout: 10000 }).should('be.visible').click();
+        cy.url().should('include', 'http://localhost:5173').then((url) => {
+            cy.log('Current URL:', url);
+        });
 
-        cy.contains('NewAudit').click();
-        cy.url().should('include', '/newAudit');
+        cy.contains('NewAudit', { timeout: 10000 }).should('be.visible').click();
+        cy.url().should('include', '/newAudit').then((url) => {
+            cy.log('Current URL:', url);
+        });
 
-        cy.contains('PerformAudit').click();
-        cy.url().should('include', '/performAudit');
+        cy.contains('PerformAudit', { timeout: 10000 }).should('be.visible').click();
+        cy.url().should('include', '/performAudit').then((url) => {
+            cy.log('Current URL:', url);
+        });
 
-        cy.contains('Evaluation').click();
-        cy.url().should('include', '/evaluation');
+        cy.contains('Evaluation', { timeout: 10000 }).should('be.visible').click();
+        cy.url().should('include', '/evaluation').then((url) => {
+            cy.log('Current URL:', url);
+        });
     });
 
     it('should navigate to the New Audit page when the first box is clicked', () => {
-        cy.get('a[href="/newAudit"]').first().click();
-        cy.url().should('include', '/newAudit');
+        cy.get('a[href="/newAudit"]', { timeout: 10000 }).should('be.visible').first().click();
+        cy.url().should('include', '/newAudit').then((url) => {
+            cy.log('Current URL:', url);
+        });
     });
 
     it('should stay on the same page when other boxes are clicked', () => {
-        cy.get('a[href="/"]').eq(1).click();
-        cy.url().should('eq', 'http://localhost:5173/');
-
-        cy.get('a[href="/"]').eq(2).click();
-        cy.url().should('eq', 'http://localhost:5173/');
-
-        cy.get('a[href="/"]').eq(3).click();
-        cy.url().should('eq', 'http://localhost:5173/');
-
-        cy.get('a[href="/"]').eq(4).click();
-        cy.url().should('eq', 'http://localhost:5173/');
-
-        cy.get('a[href="/"]').eq(5).click();
-        cy.url().should('eq', 'http://localhost:5173/');
-
-        cy.get('a[href="/"]').eq(6).click();
-        cy.url().should('eq', 'http://localhost:5173/');
-
-        cy.get('a[href="/"]').eq(7).click();
-        cy.url().should('eq', 'http://localhost:5173/');
-
-        cy.get('a[href="/"]').eq(8).click();
-        cy.url().should('eq', 'http://localhost:5173/');
-
-        cy.get('a[href="/"]').eq(9).click();
-        cy.url().should('eq', 'http://localhost:5173/');
+        for (let i = 1; i <= 9; i++) {
+            cy.get(`a[href="/"]`, { timeout: 10000 }).eq(i).should('be.visible').click();
+            cy.url().should('eq', 'http://localhost:5173/').then((url) => {
+                cy.log(`Clicked link ${i}, Current URL:`, url);
+            });
+        }
     });
 });
