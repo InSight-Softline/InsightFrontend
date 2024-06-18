2b10ed328a64c481d3837193ebd4ac88c3a45c8a
Abolfazl <abolfazl.shahbazfar@stud.htwk-leipzig.de>
2024-06-18 23:47:33 +0200

    delete cypress from eslintconfig

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -27,7 +27,6 @@ module.exports = {
     'react-refresh',
     'react',
     'react-hooks',    
-    'cypress' // Falls man Cypress benutzt
   ],
   rules: {
     'react/jsx-no-target-blank': 'off',
