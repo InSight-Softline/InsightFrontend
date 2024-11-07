bc76f1bd1e0bbb508126251c111d883e009279ae
Merlin Schuckert <merlin.schuckert@stud.htwk-leipzig.de>
2024-11-07 17:38:50 +0100

    cypress tests implemented

---
+++ frontend/src/components/Textareas/Title.cy.jsx
@@ -0,0 +1,13 @@
+describe('Title Component', () => {
+    beforeEach(() => {
+      cy.mount(<Title>Test Title</Title>);
+    });
+  
+    it('should render the title text correctly', () => {
+      cy.get('h4').should('contain.text', 'Test Title');
+    });
+  
+    it('should have centered alignment', () => {
+      cy.get('h4').should('have.css', 'text-align', 'center');
+    });
+  });
